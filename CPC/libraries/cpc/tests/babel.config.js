f52cc4c1e8532d54b215247ee0e3278e3d6c49c8
John Can Lokman <john.c.lokman@gmail.com>
2020-03-03 11:33:13 +0100

    Added support for class properties via `@babel/plugin-proposal-class-properties`

---
+++ CPC/libraries/cpc/tests/babel.config.js
@@ -0,0 +1,12 @@
+// Warning: Keep this file in .js format, as JEST can experience issues
+// when it is a .json or babelrc. ( See: https://github.com/babel/babel/issues/8711#issuecomment-421918023 )
+module.exports = {
+    // presets: [
+    //     '@babel/preset-env',
+    //     // '@babel/preset-react',
+    //     // '@babel/preset-typescript'
+    // ],
+    plugins: [
+        '@babel/plugin-proposal-class-properties'
+    ]
+};
